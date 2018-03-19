2964a9b9d1abd44752022f89abce3e73782bc0fa
LShostenko <luka.shostenko@gmail.com>
2018-03-19 11:13:02 +0200

    Speed up peak detection and calculation of unfolded nns.

--- nns/nns.py
+++ nns/nns.py
@@ -1,38 +1,74 @@
 import numpy as np
 
 
-def get_peaks(array):
-    peaks = []
+def detect_peaks(array):
+    size = len(array)
 
-    start = -1
-    cur_val = - np.inf
+    if size < 3:
+        return np.array([])
 
-    for ix, next_val in enumerate(array):
-        if next_val > cur_val:
-            start = ix
+    diffs = np.hstack((np.inf, np.diff(array), - np.inf))
 
-        elif start != -1 and next_val < cur_val:
-            peaks.append((start + ix - 1) / 2)
-            start = -1
+    raising = diffs[:-1] > 0
 
-        cur_val = next_val
+    single_peaks = np.where(raising & (diffs[1:] < 0))[0].astype(np.float64)
 
-    if peaks and start != -1:
-        peaks.append((start + ix) / 2)
+    multi_peak_candidates = np.where(raising & (diffs[1:] == 0))[0]
+    multi_peaks = []
+
+    for peak in multi_peak_candidates:
+        cur_ind = peak
+
+        while diffs[cur_ind + 1] == 0:
+            cur_ind += 1
+
+        if diffs[cur_ind + 1] < 0:
+            if peak == 0 and cur_ind == size - 1:
+                return np.array([])
+
+            multi_peaks.append((peak + cur_ind) / 2)
+
+    if not multi_peaks:
+        return single_peaks
+
+    ixs = np.searchsorted(single_peaks, multi_peaks)
+    peaks = np.insert(single_peaks, ixs, multi_peaks)
 
     return peaks
 
 
-def get_unfolded_nns(peaks, window=5):
+def moving_average(array, averaging_len=5):
+    if averaging_len <= 0 or not isinstance(averaging_len, int):
+        raise ValueError('\'averaging_len\' should be positive integer')
+
+    window = 2 * averaging_len + 1
+    size = array.size
+
+    if size < window:
+        return array
+
+    array = np.hstack(
+        (0, array[:averaging_len], array, array[size - averaging_len:]),
+    )
+
+    cumulative = np.cumsum(array)
+
+    average = cumulative[window:].astype(np.float64)
+    average -= cumulative[:- window]
+    average /= window
+
+    return average
+
+
+def get_unfolded_nns(peaks, averaging_len=None):
     spacings = np.diff(peaks)
 
-    length = spacings.shape[0]
-    unfolded_spacings = np.zeros(length)
+    if not np.all(spacings > 0):
+        raise ValueError('\'peaks\' should be sorted array of unique numbers')
 
-    for ix in range(length):
-        start = max(0, ix - window)
-        end = min(length, ix + window + 1)
+    if averaging_len is None:
+        return spacings / spacings.mean()
 
-        unfolded_spacings[ix] = spacings[ix] / spacings[start:end].mean()
+    average = moving_average(spacings, averaging_len=averaging_len)
 
-    return unfolded_spacings
+    return spacings / average
